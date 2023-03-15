f60ca78d1aa7e464f78a027ea455eb2f3b7ac810
Mateusz Pusz <mateusz.pusz@gmail.com>
2023-03-15 22:06:30 +0100

    `osyncstream` is not needed here

--- example/run_async.cpp
+++ example/run_async.cpp
@@ -21,8 +21,8 @@
 // SOFTWARE.
 
 #include <mp-coro/async.h>
-#include <mp-coro/task.h>
 #include <mp-coro/sync_await.h>
+#include <mp-coro/task.h>
 #include <concepts>
 #include <iostream>
 #include <optional>
@@ -30,8 +30,8 @@
 
 mp_coro::task<int> foo()
 {
-  const int res = co_await mp_coro::async([]{ return 42; });
-  co_await mp_coro::async([&]{ std::cout << "Result: " << res << std::endl; });
+  const int res = co_await mp_coro::async([] { return 42; });
+  co_await mp_coro::async([&] { std::cout << "Result: " << res << std::endl; });
   co_return res + 23;
 }
 
@@ -43,13 +43,13 @@ mp_coro::task<> bar()
 
 mp_coro::task<int> boo()
 {
-  const int res = co_await mp_coro::async([]{
-    std::osyncstream(std::cout) << "About to throw an exception\n";
+  const int res = co_await mp_coro::async([] {
+    std::cout << "About to throw an exception\n";
     throw std::runtime_error("Some error");
-    std::osyncstream(std::cout) << "This will never be printed\n";
+    std::cout << "This will never be printed\n";
     return 42;
   });
-  std::osyncstream(std::cout) << "I will never tell you that the result is: " << res << std::endl;
+  std::cout << "I will never tell you that the result is: " << res << std::endl;
   co_return res;
 }
 
@@ -63,12 +63,11 @@ template<typename T>
 void test(mp_coro::task<T> t)
 {
   try {
-    if constexpr(std::is_void_v<T>)
+    if constexpr (std::is_void_v<T>)
       sync_await(t);
     else
       std::cout << "Result: " << sync_await(t) << '\n';
-  }
-  catch(const std::exception& ex) {
+  } catch (const std::exception& ex) {
     std::cout << "Exception caught: " << ex.what() << "\n";
   }
 }
@@ -78,9 +77,8 @@ int main()
   try {
     test(example());
     test(boo());
-    test(mp_coro::make_task(mp_coro::async([]{ return 123; })));
-  }
-  catch(const std::exception& ex) {
+    test(mp_coro::make_task(mp_coro::async([] { return 123; })));
+  } catch (const std::exception& ex) {
     std::cout << "Unhandled exception: " << ex.what() << "\n";
   }
 }
