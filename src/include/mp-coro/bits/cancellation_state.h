de702978d2f7f049d5be55642ca15a5e51879943
Gonzalo Juarez <gonzalojuarezte@gmail.com>
2023-06-28 14:36:47 +0200

    reformat documentatin of cancellation_state.h

--- src/include/mp-coro/bits/cancellation_state.h
+++ src/include/mp-coro/bits/cancellation_state.h
@@ -7,7 +7,8 @@
 #include <utility>
 
 namespace mp_coro {
-// forward declaration of cancellation_registration
+
+// Forward declaration of cancellation_registration.
 struct cancellation_registration;
 
 namespace detail {
@@ -15,25 +16,17 @@ class [[nodiscard]] cancellation_state : noncopyable {
 public:
   static cancellation_state* make_cancellation_state() { return new cancellation_state(); };
 
-  // increments source reference count.
-  // returns previous source reference count.
   void add_source_ref() { add_ref(cancellation_source_rc_pos); }
 
-  // decrements source reference count.
-  // calls `~cancellation_state` if there are no more source references.
   void dec_source_ref() { dec_ref(cancellation_source_rc_pos); }
 
-  // increments token reference count.
   void add_token_ref() { add_ref(cancellation_token_rc_pos); }
 
-  // decrements token reference count.
-  // calls `~cancellation_state` if there are no more token references.
   void dec_token_ref() { dec_ref(cancellation_token_rc_pos); }
 
-  // Sets `cancellation_requested_flag`.
-  // Returns previous values of cancellation_requested.
+
   bool request_cancellation() noexcept
-  {
+  {  // Returns previous values of cancellation_requested.
     auto old_state = state_.fetch_or(cancellation_requested_flag);
     if (old_state & cancellation_requested_flag) return true;  // if cancellation is already requested
     // otherwise, we are the first to request cancellation
@@ -41,28 +34,32 @@ public:
     return false;
   };
 
-  // Returns true if there are source references or if cancellation has already been requested
+
   [[nodiscard]] bool can_be_cancelled() const noexcept
   {
     return state_.load(std::memory_order_acquire) & can_be_cancelled_mask;
   }
 
-  // Returns value of `cancellation_requested_flag`
+
   [[nodiscard]] bool is_cancellation_requested() const noexcept
   {
     return state_.load(std::memory_order_acquire) & cancellation_requested_flag;
   }
 
-  [[nodiscard]] bool try_register_callback([[maybe_unused]] cancellation_registration* cancellation_registration) { return false; }
+  [[nodiscard]] bool try_register_callback([[maybe_unused]] cancellation_registration* cancellation_registration)
+  {
+    if (is_cancellation_requested()) return false;
+    // TODO: implement this using cancellation_register
+    return false;
+  }
 
 private:
   ~cancellation_state() = default;
 
-  // dec_ref and add_ref factorise common implementation.
   void dec_ref(std::uint64_t cancellation_rc_pos)
   {
-    auto old_state = state_.fetch_sub(cancellation_rc_pos, std::memory_order_acq_rel);
     // If old reference count was one, then the last reference got removed. Delete this object.
+    auto old_state = state_.fetch_sub(cancellation_rc_pos, std::memory_order_acq_rel);
     if ((old_state & clean_rc_mask) == cancellation_rc_pos) delete this;
   }
 
@@ -73,27 +70,34 @@ private:
     state_.fetch_add(cancellation_rc_pos, std::memory_order_relaxed);
   }
 
+  // Initialized with source reference count set to 1.
   std::atomic<std::uint64_t> state_ = cancellation_source_rc_pos;
 
-  // State can be seen as below, but packed in a single atomic.
-  // struct state {
+  // State and its default initialized value is a bit packed version of the following struct:
+  // struct {
   //   bool cancellation_requested: 1 = false;                   <-- bit 0
   //   bool cancellation_notification_complete: 1 = false;       <-- bit 1
   //   std::uint32_t source_ref_count: 31 = 1;                   <-- bits 2 to 32
   //   std::uint32_t token_ref_count: 31 = 0;                    <-- bits 33 to 63
   // } state_;
 
+  // Constants used to handle state:
+  // - `cancellation_requested_flag`: indicates that a cancellation request has already been issued.
+  // - `cancellation_notification_complete_flag`: Indicates that all tokens have been notified of cancellation and all
+  // callbacks have been called.
+  // - `cancellation_source_rc_pos` and `cancellation_token_rc_pos`: Bit positions of the reference counting for
+  // cancellation_source and cancellation_token/cancellation_registration. clean_rc_mask: Mask used to dispose bits that
+  // do not correspond to reference counting.
+  // - `can_be_cancelled`: Mask used to indicate if cancellation_state is cancellable. State is cancellable if either
+  // source refcount > 1, token/register refcount > 0, cancellation has been requested, or cancellation completion has
+  // already been notified.
   static constexpr std::uint64_t cancellation_requested_flag = UINT64_C(1);
-  // Flag indicating all tokens have been notified of cancellation.
-  static constexpr std::uint64_t cancellation_notification_complete_flag = UINT64_C(1) << 1u;
-  // Start positions of reference counting for cancellation_source and cancellation_tokens.
-  static constexpr std::uint64_t cancellation_source_rc_pos = UINT64_C(1) << 2u;
-  static constexpr std::uint64_t cancellation_token_rc_pos = UINT64_C(33) << 33u;
-  // Mask used to dispose bits that do not correspond to reference counting.
+  static constexpr std::uint64_t cancellation_notification_complete_flag = UINT64_C(1) << 1;
+  static constexpr std::uint64_t cancellation_source_rc_pos = UINT64_C(1) << 2;
+  static constexpr std::uint64_t cancellation_token_rc_pos = UINT64_C(33) << 33;
+  static constexpr std::uint64_t can_be_cancelled_mask = cancellation_token_rc_pos - 1;
   static constexpr std::uint64_t clean_rc_mask =
     ~(cancellation_requested_flag | cancellation_notification_complete_flag);
-  // State is cancellable if source ref > 1 or cancellation requested or notification complete
-  static constexpr std::uint64_t can_be_cancelled_mask = cancellation_token_rc_pos - 1;
 };
 
 
@@ -162,6 +166,4 @@ struct source_cancellation_state_ptr final : cancellation_state_ptr_base<source_
 // TODO when callback registration is implemented:
 // - `try_register_callback` must be implemented.
 // - `request_cancellation` should invoke callbacks.
-// - `can_be_cancelled` can also return true if registered_callbacks_count > 0.
-// - `cancellation_notification_complete_flag` also indicates that all callbacks have been called.
 // - `~cancellation_state` should free resources kept to hold registered callbacks
